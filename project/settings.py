1904f33b630768d687b5d019e29ccd1afed69b48
José Antonio <joseantonioalmeida217@gmail.com>
2026-03-27 18:56:15 -0300

    configurando arquivos statics e static root para collect static

--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,3 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# python manage.py collectstatic
+STATIC_ROOT = BASE_DIR / 'static'
