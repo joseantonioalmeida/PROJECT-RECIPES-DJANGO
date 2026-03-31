e7f6775c10b0564b828a5afacef7a2d2011e97df
José Antonio <joseantonioalmeida217@gmail.com>
2026-03-30 21:32:15 -0300

    usando o model Recipe real(com QuerySet) na view home

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from utils.recipes.factoty import make_recipe
+from recipes.models import Recipe
+
 
 # Create your views here.
 
 
 def index(request):
-
+    recipes = Recipe.objects.all().order_by('-id')
     context = {
         'name': 'José Antonio',
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    }
+    return render(
+        request,
+        'recipes/pages/home.html',
+        context=context,
+    )
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    context = {
+        'name': 'José Antonio',
+        'recipes': recipes,
     }
     return render(
         request,
