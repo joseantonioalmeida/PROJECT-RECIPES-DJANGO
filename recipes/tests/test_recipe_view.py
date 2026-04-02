eeaf6c89777af2e40ac553236de68ba7efa85458
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-02 18:40:33 -0300

    criando fixtures pra um teste único (Recipe, Category, Author)

--- recipes/tests/test_recipe_view.py
+++ recipes/tests/test_recipe_view.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from recipes import views
+from recipes import views, models
+from django.contrib.auth.models import User
+
 
 class RecipeViewsTest(TestCase):
     #home
@@ -22,6 +24,31 @@ class RecipeViewsTest(TestCase):
             'No recipes found here',
             response.content.decode('utf-8'),
         )
+    
+    def test_recipe_home_loads_recipes(self):
+        category = models.Category.objects.create(name='Category Test')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='user',
+            username='user',
+            password='user',
+            email='user',
+        )
+        recipe = models.Recipe.objects.create(
+            category=category,
+            author=author,
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe Preparation Steps',
+            preparation_steps_is_html=False,
+            is_published=True,
+        )
+        assert 1 == 1
 
     #category
     def test_recipe_category_view_function_is_corret(self):
