1cade96234a751c2c684042ae133d324940a3987
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-03 21:20:09 -0300

    adicionando coverage (cobertura de test)

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -29,7 +29,7 @@ class RecipeModelTest(RecipeTestBase):
         ('title', 65),
         ('description', 165),
         ('preparation_time_unit', 65),
-        ('servings_unitt', 65),
+        ('servings_unit', 65),
     ])
     def test_recipe_fields_max_length(self, field, max_length):
         setattr(self.recipe, field, 'A' * (max_length + 1))
