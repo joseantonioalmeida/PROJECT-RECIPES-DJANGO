63e4a5fd1bcfc0837a91dc8f82432d87004b3f70
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-08 21:25:36 -0300

    levantando erro 404 se o termo de busca não for enviado

--- recipes/views.py
+++ recipes/views.py
@@ -56,6 +56,10 @@ def recipe(request, id):
     )
 
 def search(request):
+    search_term = request.GET.get('q')
+
+    if not search_term:
+        raise Http404()
     return render(
         request,
         'recipes/pages/search.html'
