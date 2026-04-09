ad37e0a69f65e6ce3ae5146eb7127fc07794a43b
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-09 14:37:35 -0300

    separando os testes de todas as  views em grupos separados (Test Suites)

---
+++ recipes/tests/test_recipe_detail_view.py
@@ -0,0 +1,40 @@
+from django.urls import reverse, resolve
+from recipes import views
+from .test_recipe_base import RecipeTestBase
+   
+
+class RecipeDetailViewTest(RecipeTestBase):     
+    #recipe
+    def test_recipe_recipe_view_function_is_correct(self):
+        view =  resolve(reverse('recipes:recipe', kwargs={'id':1}))
+        self.assertIs(view.func, views.recipe)
+
+    def test_recipe_recipe_view_returns_404_if_no_recipes_found(self):
+        response = self.client.get(
+            reverse("recipes:recipe",
+                    kwargs={'id': 1000}))
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_template_loads_the_correct_recipe(self):
+        needed_title = "This is a detail page - It load one recipe"
+        #cria uma receita
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id':1}))
+        response_content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipe']
+
+        self.assertIn(needed_title, response_content)
+
+        assert 1 == 1
+
+    def test_recipe_detail_template_dont_load_recipe_not_published(self):
+        """Test recipe is_published False dont show"""
+        #cria uma receita
+        recipe = self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse(
+            'recipes:recipe', 
+            kwargs={
+                'id':recipe.id})) #type:ignore
+        self.assertEqual(response.status_code, 404)
