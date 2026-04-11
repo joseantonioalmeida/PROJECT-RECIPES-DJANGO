13d14b32ef66aac33f4db62f180f0061b6882e33
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-11 15:57:15 -0300

    criando um formulário para o usuário se registar usando o model padrão User

--- authors/views.py
+++ authors/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
+from .forms import RegisterForm
 # Create your views here.
 
 def register_view(request):
+    form = RegisterForm()
     return render(
         request,
-        'authors/pages/register_view.html'
+        'authors/pages/register_view.html',
+        {
+            'form': form,
+        
+        }
     )
