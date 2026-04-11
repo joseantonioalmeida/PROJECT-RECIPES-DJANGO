069f2bbdfbd8d44e75e43a382da7f366337761b0
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-11 16:19:33 -0300

    adicionando todos os campos manualmente para melhor controle do css

--- authors/views.py
+++ authors/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .forms import RegisterForm
-# Create your views here.
+
+
 
 def register_view(request):
-    form = RegisterForm()
+    if request.POST:
+        form = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
     return render(
         request,
         'authors/pages/register_view.html',
