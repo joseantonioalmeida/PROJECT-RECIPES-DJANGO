24c27afcc85e4642bbfe241c5945f28627ab3d29
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-15 11:17:28 -0300

    Reorganizando os arquivos da aplicação.

--- project/settings.py
+++ project/settings.py
@@ -126,6 +126,10 @@ STATIC_URL = 'static/'
 # python manage.py collectstatic
 STATIC_ROOT = BASE_DIR / 'static'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
