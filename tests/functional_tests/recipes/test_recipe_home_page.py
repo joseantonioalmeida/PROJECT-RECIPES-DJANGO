4d3dd915d8abb5520206604a487ea9eb624e897d
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-17 11:04:50 -0300

    agrupando os testes com pytest markers

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,9 +1,15 @@
 from selenium.webdriver.common.by import By
 from .base import RecipeBasePageFunctionalTest
+import pytest
 
+
+@pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBasePageFunctionalTest):
-
-    def test_recipe_home_page_loads(self):
+    def test_recipe_home_page_without_recipes_not_found_message(self):
+        self.browser.get(self.live_server_url)
+        body = self.browser.find_element(By.TAG_NAME, 'body')
+        self.assertIn("No recipes found here", body.text)
+    def test_recipe1_home_page_without_recipes_not_found_message(self):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn("No recipes found here", body.text)
