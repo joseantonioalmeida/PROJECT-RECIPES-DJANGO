ba16939c98a33bac36a2028a5bb65d97f866a93a
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-19 10:43:44 -0300

    testando o logout completo

---
+++ authors/tests/test_author_logout.py
@@ -0,0 +1,43 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+class AuthorLogoutTest(TestCase):
+    def test_user_tries_to_logout_using_get_methor(self):
+        User.objects.create_user(username='my_user', password='my_password')
+        self.client.login(username='my_user', password='my_password')
+        
+        response = self.client.get(reverse('authors:logout'), follow=True)
+        
+        self.assertIn('Invalid logout request',response.content.decode('utf-8'))
+
+    
+    def test_user_tries_to_logout_another_user(self):
+        User.objects.create_user(username='my_user', password='my_password')
+        self.client.login(username='my_user', password='my_password')
+        
+        response = self.client.post(
+            reverse('authors:logout'),
+            data={
+                'username': 'other_user',
+            },
+            follow=True
+        )
+        
+        self.assertIn('Invalid logout user',response.content.decode('utf-8'))
+
+    
+    def test_user_can_logout_successfully(self):
+        User.objects.create_user(username='my_user', password='my_password')
+        self.client.login(username='my_user', password='my_password')
+        
+        response = self.client.post(
+            reverse('authors:logout'),
+            data={
+                'username': 'my_user',
+            },
+            follow=True
+        )
+        
+        self.assertIn('You are logged out.',response.content.decode('utf-8'))
+
