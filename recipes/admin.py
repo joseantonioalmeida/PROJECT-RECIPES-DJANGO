a5761d571f330440fd4f1851eeb2d7679c0321a5
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-19 14:09:53 -0300

    ajustando a admin do django e preenchendo o slug automaticamente com prepopulated_fields na admin padrão

--- recipes/admin.py
+++ recipes/admin.py
@@ -3,10 +3,17 @@ from .models import Category,Recipe
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    ...
+        ...
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    ...
-
-# Register your models here.
+    list_display = ('id', 'title', 'created_at','is_published' , 'author')
+    list_display_links = ('title', 'created_at' )
+    search_fields = ('id', 'title', 'description', 'slug', 'preparation_steps')
+    list_filter = ('category','author','is_published',)
+    list_per_page = 10
+    list_editable = 'is_published',
+    ordering = '-id',
+    prepopulated_fields = {
+          "slug": ('title',)
+    }
