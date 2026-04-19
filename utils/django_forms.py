1ce12778797505ae0abc5b7688a55b8369a18b74
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-19 15:23:32 -0300

    corrigindo o css do formulário e da grid

--- utils/django_forms.py
+++ utils/django_forms.py
@@ -1,6 +1,11 @@
 import re
 from django.core.exceptions import ValidationError
 
+
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
+
 def add_placeholder(field, placeholder_val):
     field.widget.attrs['placeholder'] = placeholder_val
 
