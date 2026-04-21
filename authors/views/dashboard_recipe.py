6175a6fb3b41e7f8c7a44a8c092333193df9b7cf
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-21 19:05:39 -0300

    Reutilizando a class based view na herança para deletar a recipe

--- authors/views/dashboard_recipe.py
+++ authors/views/dashboard_recipe.py
@@ -63,3 +63,15 @@ class DashboardRecipe(View):
             return redirect(reverse('authors:dashboard_recipe_edit', args=(recipe_obj.id,)))
         
         return self.render_recipe(form)       
+    
+
+@method_decorator(
+    login_required(login_url='authors:login', redirect_field_name='next'),
+    name='dispatch',
+)
+class DashboardRecipeDelete(DashboardRecipe):
+    def post(self, *args, **kwargs):
+        recipe = self.get_recipe(self.request.POST.get('id'))
+        recipe.delete() # type:ignore
+        messages.success(self.request, 'Recipe deleted successfully.')
+        return redirect(reverse('authors:dashboard'))
