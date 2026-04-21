64e167af5a96b0695fb54fd9ff99bcd66422ff82
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-20 23:05:45 -0300

    Validando o formulário de cadastrar e editar.

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -1,11 +1,18 @@
 from django import forms
 from recipes.models import Recipe
 from utils.django_forms import add_attr
+from collections import defaultdict
+from django.core.exceptions import ValidationError
+from utils.strings import is_positive_number
+
 
 
 class AuthorRecipeForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self._my_errors = defaultdict(list)
+
         add_attr(self.fields.get('preparation_steps'), 'class', 'span-2')
         add_attr(self.fields.get('title'), 'class', 'span-2')
 
@@ -33,3 +40,45 @@ class AuthorRecipeForm(forms.ModelForm):
                 )
             )
         }
+    
+    def clean(self, *args, **kwargs):
+        super_clean = super().clean(*args, **kwargs)
+        cleaned_data = self.cleaned_data
+        title = cleaned_data.get('title')
+
+        description = cleaned_data.get('description')
+
+
+        if title== description:
+            self._my_errors['title'].append('Cannot be equal to description')
+            self._my_errors['description'].append('Cannot be equal to title')
+
+
+        if self._my_errors:
+            raise ValidationError(self._my_errors) #type:ignore
+
+        return super_clean
+    
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        
+        if len(title) < 5: #type:ignore
+            self._my_errors['title'].append('Must have at least 5 chars.')
+        
+        return title
+    
+    def clean_preparation_time(self):
+        preparation_time = self.cleaned_data.get('preparation_time')
+        
+        if not is_positive_number(preparation_time):
+            self._my_errors['preparation_time'].append('Must be a positive number.')
+        
+        return preparation_time
+    
+    def clean_servings(self):
+        servings = self.cleaned_data.get('servings')
+        
+        if not is_positive_number(servings):
+            self._my_errors['servings'].append('Must be a positive number.')
+        
+        return servings
