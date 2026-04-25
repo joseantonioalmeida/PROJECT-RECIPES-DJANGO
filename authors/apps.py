f915e39d49d5790281192f451eda33c532671bf0
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-24 21:11:01 -0300

    Criando um Signal para ser emitido quando um usuário for criado (post_save)

--- authors/apps.py
+++ authors/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class AuthorsConfig(AppConfig):
     name = 'authors'
+
+    def ready(self, *args, **kwargs) -> None:
+        import authors.signals
+        super_ready = super().ready(*args, **kwargs)
+        return super_ready
