381ab2cc831c0497b218d081a472fdb3cfea5394
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-28 11:18:50 -0300

    Enviando dados para serializer no django rest framework

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -3,14 +3,25 @@ from rest_framework.response import Response
 from recipes.models import Recipe
 from recipes.serializers import RecipeSerializer
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 
 
 
-@api_view()
+@api_view(http_method_names=['GET', 'POST'])
 def recipe_api_list(request):
-    recipes = Recipe.objects.get_published()[:20]
-    serializer = RecipeSerializer(instance=recipes, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        recipes = Recipe.objects.get_published()[:20]
+        serializer = RecipeSerializer(instance=recipes, many=True)
+        return Response(serializer.data)
+     
+    elif request.method == 'POST':  
+        serializer = RecipeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            serializer.validated_data, 
+            status=status.HTTP_201_CREATED
+        )
 
 @api_view()
 def recipe_api_detail(request, pk):
