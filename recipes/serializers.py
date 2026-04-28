0da82facbde0a7d76a17ff7ca81f5014efbb8c66
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-27 22:12:53 -0300

    migrando para modelSerializer ao invés de serializer no django rest framework

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,18 +1,18 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from recipes.models import Recipe
 
-class RecipeSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=25655)
-    description = serializers.CharField(max_length=255)
-    public = serializers.BooleanField(source='is_published')
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'description', 'public', 
+                  'preparation', 'author', 'category'
+        ]
+    public = serializers.BooleanField(source='is_published', read_only=True)
     preparation = serializers.SerializerMethodField(
-        method_name='any_method_name'
+        method_name='any_method_name', read_only=True,
     )
-    category = serializers.StringRelatedField()
-    author = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all(),
-    )
+    category = serializers.StringRelatedField(read_only=True)
 
     def any_method_name(self, recipe):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
--- recipes/views/api.py
+++ recipes/views/api.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from recipes.models import Recipe
 from recipes.serializers import RecipeSerializer
 from django.shortcuts import get_object_or_404
-from rest_framework import status
 
 
 
