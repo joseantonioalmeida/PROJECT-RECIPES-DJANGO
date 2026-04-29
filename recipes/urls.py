5193ec0940b6bd10a1e4a4e8583f9bbca40d73d8
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-29 17:32:16 -0300

    Router, urlpatterns, path e include no urls.py do django rest framework

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import SimpleRouter
 
 app_name = 'recipes'
+
+recipe_api_v2_router = SimpleRouter()
+recipe_api_v2_router.register(
+    'recipes/api/v2',
+    views.RecipeAPIv2ViewSet,
+    basename='recipes-api'
+)
 
 urlpatterns = [
     path("", views.RecipeListViewHome.as_view(), name="home"),
@@ -9,14 +17,8 @@ urlpatterns = [
     path('recipes/category/<int:category_id>/', views.RecipeListViewCategory.as_view(), name="category"),
     path('recipes/<int:pk>/', views.RecipeDetail.as_view(), name="recipe"),
     path('recipes/api/v1/', views.RecipeListViewHomeApi.as_view(), name="recipe_api_v1"),
-    path('recipes/api/v1/<int:pk>/', views.RecipeDetailViewApi.as_view(), name="recipe_api_v1_detail"),
-    path('recipes/api/v2/', views.RecipeAPIv2ViewSet.as_view({
-        'get': 'list',
-        'post': 'create',
-    }), name="recipe_api_v2"),
-    path('recipes/api/v2/<int:pk>/', views.RecipeAPIv2ViewSet.as_view({
-        'get': 'retrieve',
-        'patch': 'partial_update',
-        'delete': 'destroy',
-    }), name="recipe_api_v2_detail"),
+    path('recipes/api/v1/<int:pk>/', views.RecipeDetailViewApi.as_view(), name="recipe_api_v1_detail"),  
+    path(
+        "", include(recipe_api_v2_router.urls))
+    
 ]
