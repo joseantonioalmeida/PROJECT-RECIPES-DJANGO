dc8e708d0ebaa30fb0351e1140550bdbc41f327d
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-29 16:46:52 -0300

    usando RetriveUpdateDestroyAPIView e sobrescrevendo métodos para customização

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -4,7 +4,7 @@ from recipes.serializers import RecipeSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.pagination import PageNumberPagination
 
 
@@ -17,30 +17,21 @@ class RecipeAPIv2List(ListCreateAPIView):
     pagination_class = RecipeAPIv2Pagination
 
 
-class RecipeAPIv2Detail(APIView):
-    def get_recipe(self, pk):
-        recipe = get_object_or_404(
-            Recipe.objects.get_published(), #type:ignore
-            pk=pk,
-        )
-        return recipe
-    
-    def get(self, request, pk):
-        recipe = self.get_recipe(pk)
-        serializer = RecipeSerializer(instance=recipe, many=False)
-        return Response(serializer.data)
-    
-    def patch(self, request, pk):
-        recipe = self.get_recipe(pk)
+class RecipeAPIv2Detail(RetrieveUpdateDestroyAPIView):
+    queryset = Recipe.objects.get_published() #type:ignore
+    serializer_class = RecipeSerializer
+    pagination_class = RecipeAPIv2Pagination
+
+    def patch(self, request, *args, **kwargs):
+        recipe = self.get_queryset().filter(pk=kwargs.get('pk')).first()
         serializer = RecipeSerializer(
-            instance=recipe, data=request.data, 
-            many=False, partial=True
+            instance=recipe,
+            data=request.data,
+            many=False,
+            partial=True,
         )
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
-    
-    def delete(self, request, pk):
-        recipe = self.get_recipe(pk)
-        recipe.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(
+            serializer.data,
+        )
