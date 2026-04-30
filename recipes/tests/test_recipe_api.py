5eced4dd8249e311f794e6fe01ef3c41b9096420
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-30 20:33:23 -0300

    testando receitas se receitas não publicada realmente não serão exibidas

--- recipes/tests/test_recipe_api.py
+++ recipes/tests/test_recipe_api.py
@@ -5,9 +5,13 @@ from unittest.mock import patch
 
 
 class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
-    def test_recipe_api_list_returns_status_code_200(self):
+    def get_recipe_api_list(self):
         api_url = reverse('recipes:recipes-api-list')
-        response = self.client.get(api_url)
+        response = self.client.get(api_url)   
+        return response
+    
+    def test_recipe_api_list_returns_status_code_200(self):
+        response = self.get_recipe_api_list()
         self.assertEqual(
             response.status_code,
             200
@@ -17,9 +21,21 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
     def test_recipe_api_list_loads_correct_number_of_recipes(self):
         wanted_number_of_recipes = 7
         self.make_recipe_in_batch(wanted_number_of_recipes)
-        response = self.client.get(reverse('recipes:recipes-api-list'))
+        response = self.get_recipe_api_list()
         qtd_of_load_recipes = len(response.data.get('results')) #type:ignore
         self.assertEqual(
             qtd_of_load_recipes,
             wanted_number_of_recipes
+        )
+
+    def test_recipe_api_list_do_not_show_published_recipes(self):
+        recipes = self.make_recipe_in_batch(2)
+        recipe_not_published = recipes[0]
+        recipe_not_published.is_published = False
+        recipe_not_published.save()
+        recipe1 = recipes[1]
+        response = self.get_recipe_api_list()
+        self.assertEqual(
+            response.data.get('results'), #type:ignore
+            1
         )
