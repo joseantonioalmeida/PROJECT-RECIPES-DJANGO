b106522707cb5959685bd68426b8197fb81e1f86
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-30 12:16:25 -0300

    usei o request.user para quando criar uma receita, criar com o usuario

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -6,6 +6,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from recipes.permissions import IsOwner
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 
 
 class RecipeAPIv2Pagination(PageNumberPagination):
@@ -37,6 +38,17 @@ class RecipeAPIv2ViewSet(ModelViewSet):
         if self.request.method in ['PATCH', 'DELETE']:
             return [IsOwner(),]
         return super().get_permissions()
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, 
+            status=status.HTTP_201_CREATED, 
+            headers=headers
+        )
 
     def partial_update(self, request, *args, **kwargs):
         recipe = self.get_object()
