af0e9830a98de84c0f69f11cf59a18315efd9d00
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-30 21:48:41 -0300

    criando um método pra obter o JWT Access Token no django rest framework

--- recipes/tests/test_recipe_api.py
+++ recipes/tests/test_recipe_api.py
@@ -14,6 +14,18 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
         response = self.client.get(api_url)   
         return response
     
+    def get_jwt_access_token(self):
+        userdata:dict = {
+            'username': 'user',
+            'password': 'password'
+        }
+        author =self.make_author(
+            username=userdata.get('username', ''),
+            password=userdata.get('password', '')
+        )
+        response = self.client.post(reverse('recipes:token_obtain_pair'),data={**userdata})
+        return response.data.get('access') #type:ignore
+    
     def test_recipe_api_list_returns_status_code_200(self):
         response = self.get_recipe_api_list()
         self.assertEqual(
@@ -79,3 +91,6 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
             response.status_code,
             401
         )
+    
+    def test_jwt_login(self):
+        print(self.get_jwt_access_token())
