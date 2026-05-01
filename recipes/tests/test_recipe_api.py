751153f7bd9cf05b27a5e64dfc8916e1f6d321a0
José Antonio <joseantonioalmeida217@gmail.com>
2026-04-30 21:00:37 -0300

    testando se podemos carregar receitas por category no django rest framework

--- recipes/tests/test_recipe_api.py
+++ recipes/tests/test_recipe_api.py
@@ -5,8 +5,8 @@ from unittest.mock import patch
 
 
 class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
-    def get_recipe_api_list(self):
-        api_url = reverse('recipes:recipes-api-list')
+    def get_recipe_api_list(self, reverse_result=None):
+        api_url = reverse_result or reverse('recipes:recipes-api-list')
         response = self.client.get(api_url)   
         return response
     
@@ -38,4 +38,32 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
         self.assertEqual(
             response.data.get('results'), #type:ignore
             1
+        )
+
+    @patch('recipes.views.api.RecipeAPIv2Pagination.page_size', new=10)
+    def test_recipe_api_list_loads_recipes_by_category_id(self):
+        # Cria as categorias
+        category_wanted = self.make_category(name='WANTED_CATEGORY')
+        category_not_wanted = self.make_category(name='NOT_WANTED_CATEGORY')
+        # cria 10 receitas
+        recipes = self.make_recipe_in_batch(10)
+        # muda todas as receitas
+        for cont, recipe in enumerate(recipes):
+            if cont == 0:
+                # muda a category para não desejada
+                recipes[0].cagetory = category_not_wanted
+            else:
+                # muda o restante das recipes com a category desejada
+                recipe.category = category_wanted
+            recipe.save()
+
+        # ação: pega as receitas pela category_id desejada
+        api_url = reverse('recipes:recipes-api-list') + \
+            f'?category_id={category_wanted.id}' #type:ignore
+        response = self.get_recipe_api_list(reverse_result=api_url)
+
+        # nós devemos ver apenas as receitas que tenha a category desejada
+        self.assertEqual(
+            len(response.data.get('results')), #type:ignore
+            9
         )
